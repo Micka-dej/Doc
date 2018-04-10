647fb58ee36db7c37d5bcbcbfde67432316dd847
Draktors <enzo.hespel@hotmail.fr>
2018-04-10 23:20:44 +0200

    modal done

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,6 @@
 const login = document.querySelector('.login');
 const modal = document.querySelector('.modalLogin');
+const close = document.querySelector('.crossImg');
 
 login.addEventListener('click',function(){
     modal.style.display = 'flex';
@@ -8,4 +9,7 @@ window.addEventListener('click',function(event){
     if (event.target === modal) {
         modal.style.display = "";
     }
+});
+close.addEventListener('click',function () {
+    modal.style.display = "";
 });
