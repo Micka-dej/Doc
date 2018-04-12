fab5d264a2f2882ec7aa53205ab87af332c6021b
Draktors <enzo.hespel@hotmail.fr>
2018-04-12 10:25:10 +0200

    shoppingBag all pages

--- phpp/login.php
+++ phpp/login.php
@@ -22,9 +22,47 @@
                 <img src="./assets/imgs/shopping-bag.svg" alt="" class="shoppingBagImg">
             </div>
 
-            <div class="modalShoppingBag">
+            <<div class="modalShoppingBag">
                 <div class="modalShoppingBag-content">
+                    <img src="./assets/imgs/cross.svg" alt="" class="crossImg">
+                    <div class="modalShoppingBag-product">
+                        <div class="modalShoppingBag-left">
+                            <div class="modalShoppingBag-head" >
+                                <p class="modalShoppingbag-title">Gecko</p>
+                                <img src="./assets/imgs/shoppingBagStars.svg" alt="" class="modalShoppingbag-img">
+                            </div>
+                            <div class="modalShoppingBag-sub">
+                                <div class="modalShoppingBag-textContent">
+                                    <p class="modalShoppingBag-text">La cigarette la plus apprécié selon nos utilisateurs.</p>
+                                </div>
+                                <div class="modalShoppingBag-priceContent"><span class="modalShoppingBag-price">69 €</span></div>
+                            </div>
+                        </div>
+                        <div class="modalShoppingBag-right">
+                            <button class="modalShoppingBag-btn">paiement</button>
+                            <button class="modalShoppingBag-btn">supprimer</button>
+                        </div>
+                    </div>
 
+                    <div class="modalShoppingBag-product">
+                        <div class="modalShoppingBag-left">
+                            <div class="modalShoppingBag-head" >
+                                <p class="modalShoppingbag-title">Gecko</p>
+                                <img src="./assets/imgs/shoppingBagStars.svg" alt="" class="modalShoppingbag-img">
+                            </div>
+                            <div class="modalShoppingBag-sub">
+                                <div class="modalShoppingBag-textContent">
+                                    <p class="modalShoppingBag-text">La cigarette la plus apprécié selon nos utilisateurs.</p>
+                                </div>
+                                <div class="modalShoppingBag-priceContent"><span class="modalShoppingBag-price">69 €</span></div>
+                            </div>
+                        </div>
+                        <div class="modalShoppingBag-right">
+                            <button class="modalShoppingBag-btn">paiement</button>
+                            <button class="modalShoppingBag-btn">supprimer</button>
+                        </div>
+                    </div>
+                    <p class="totalPrice">TOTAL : 168 €</p>
                 </div>
             </div>
 
